760ee2b773ce36116c54dbf43764127862c3f5d2
Jessica Banny <98422267+jssbnn@users.noreply.github.com>
2022-03-31 15:01:10 -0400

    Add files via upload

---
+++ script9.js
@@ -0,0 +1,8 @@
+// Script for Chapter 9 Practice
+"use strict";
+
+document.getElementById("bottom").textContent = "This is the end of this document.";
+
+document.getElementsByClassName("main")[1].innerHTML = "Here is my paragraph. <br/> Don't you love it";
+
+document.getElementsByTagName("h1")[0].style.color = "green";
